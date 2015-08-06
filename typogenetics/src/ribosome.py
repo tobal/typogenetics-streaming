aab2e404542c5e9376738d8b4c388896955d0526
Tobal <tobal17@gmail.com>
2015-08-06 19:36:36 +0200

    typogenetics: added ribosome skeleton
    Signed-off-by: Tobal <tobal17@gmail.com>

---
+++ typogenetics/src/ribosome.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+class Ribosome():
+    pass
+
+if __name__ == '__main__':
+    pass
+
---
+++ typogenetics/tests/test_ribosome.py
@@ -0,0 +1,5 @@
+from unittest import TestCase
+
+class RibosomeTest(TestCase):
+    pass
+
