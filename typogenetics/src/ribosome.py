a04bbdc4872bdadbaaa532cdbb00a17562c32cc2
Tobal <tobal17@gmail.com>
2015-09-05 18:02:18 +0200

    ribosome: add conversion of one pair to mnemonic
    Signed-off-by: Tobal <tobal17@gmail.com>

--- typogenetics/src/ribosome.py
+++ typogenetics/src/ribosome.py
@@ -1,14 +1,36 @@
 #!/usr/bin/env python
 
-class Ribosome():
-    pass
+from enum import Enum
 
 class WorkingEnzyme():
     strand_pair = None
     head_pos = None
 
-class AminoAcid():
-    pass
+class AminoAcid(Enum):
+    cut = 'AC'
+    dlt = 'AG'
+    swi = 'AT'
+    mvr = 'CA'
+    mvl = 'CC'
+    cop = 'CG'
+    off = 'CT'
+    ina = 'GA'
+    inc = 'GC'
+    ing = 'GG'
+    int = 'GT'
+    tpy = 'TA'
+    rpu = 'TC'
+    lpy = 'TG'
+    lpu = 'TT'
+
+def ribosome_get_amino_acids_from_strands(strand):
+    if not strand:
+        return []
+    acids = []
+    for acid in AminoAcid:
+        if strand == acid.value:
+            acids.append(acid)
+    return acids
 
 if __name__ == '__main__':
     pass
--- typogenetics/tests/test_ribosome.py
+++ typogenetics/tests/test_ribosome.py
@@ -1,7 +1,22 @@
+
+from src.ribosome import ribosome_get_amino_acids_from_strands, AminoAcid
 from unittest import TestCase
 
 class RibosomeTest(TestCase):
-    def test_init(self):
-        from nose.tools import set_trace
-        set_trace()
+    def test_nullstrand_returns_no_amino_acids(self):
+        strand = ''
+        amino_acids = ribosome_get_amino_acids_from_strands(strand)
+        self.assertEqual(len(amino_acids), 0)
 
+    def test_can_convert_one_pair_to_amino_acid(self):
+        strand = 'AC'
+        amino_acids = ribosome_get_amino_acids_from_strands(strand)
+        self.assertEqual(len(amino_acids), 1)
+        self.assertEqual(amino_acids[0], AminoAcid.cut)
+        self.assertEqual(amino_acids[0].value, strand)
+
+    def test_converts_long_strands_to_correct_acid_chain(self):
+        pass
+
+    def test_can_cut_into_multiple_acid_chains(self):
+        pass
