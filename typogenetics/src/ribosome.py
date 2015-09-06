1f4b9ceaa36372eafdf2c65989fdc5cec991460c
Tobal <tobal17@gmail.com>
2015-09-06 12:30:20 +0200

    refactor(ribosome): make generator yield acid chains
    Signed-off-by: Tobal <tobal17@gmail.com>

--- typogenetics/src/ribosome.py
+++ typogenetics/src/ribosome.py
@@ -33,9 +33,15 @@ def ribosome_get_amino_acids_from_strands(strand):
     if not strand:
         return
     length = len(strand) // 2
+    acid_chain = []
     for i in range(length):
         pair = strand[i*2:(i+1)*2]
-        yield AminoAcid.get_acid_by_pair(pair)
+        if pair == 'AA':
+            yield acid_chain
+            acid_chain = []
+        else:
+            acid_chain.append(AminoAcid.get_acid_by_pair(pair))
+    yield acid_chain
 
 if __name__ == '__main__':
     pass
--- typogenetics/tests/test_ribosome.py
+++ typogenetics/tests/test_ribosome.py
@@ -12,22 +12,25 @@ class RibosomeTest(TestCase):
         strand = 'AC'
         amino_acids = list(ribosome_get_amino_acids_from_strands(strand))
         self.assertEqual(len(amino_acids), 1)
-        self.assertEqual(amino_acids[0], AminoAcid.cut)
-        self.assertEqual(amino_acids[0].value, strand)
+        self.assertEqual(len(amino_acids[0]), 1)
+        self.assertEqual(amino_acids[0][0], AminoAcid.cut)
+        self.assertEqual(amino_acids[0][0].value, strand)
 
     def test_converts_long_strands_to_correct_acid_chain(self):
         strand = 'ACGCCACC'
         amino_acids = list(ribosome_get_amino_acids_from_strands(strand))
-        self.assertEqual(len(amino_acids), 4)
-        self.assertListEqual(amino_acids, [AminoAcid.cut, AminoAcid.inc,
-                                           AminoAcid.mvr, AminoAcid.mvl])
+        self.assertEqual(len(amino_acids), 1)
+        self.assertEqual(len(amino_acids[0]), 4)
+        self.assertListEqual(amino_acids[0], [AminoAcid.cut, AminoAcid.inc,
+                                              AminoAcid.mvr, AminoAcid.mvl])
 
     def test_converts_long_strands_with_extra_character(self):
         strand = 'ACGCCACCG'
         amino_acids = list(ribosome_get_amino_acids_from_strands(strand))
-        self.assertEqual(len(amino_acids), 4)
-        self.assertListEqual(amino_acids, [AminoAcid.cut, AminoAcid.inc,
-                                           AminoAcid.mvr, AminoAcid.mvl])
+        self.assertEqual(len(amino_acids), 1)
+        self.assertEqual(len(amino_acids[0]), 4)
+        self.assertListEqual(amino_acids[0], [AminoAcid.cut, AminoAcid.inc,
+                                              AminoAcid.mvr, AminoAcid.mvl])
 
     def test_can_cut_into_multiple_acid_chains(self):
         strand = 'ACGCAACACCG'
