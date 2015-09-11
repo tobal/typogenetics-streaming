541619187526cd8d85c3d7c3b4822d46816a86cb
Tobal <tobal17@gmail.com>
2015-09-11 14:46:23 +0200

    feat(aminoacid): add tertiary direction to amino acids
    Signed-off-by: Tobal <tobal17@gmail.com>

--- typogenetics/aminoacid.py
+++ typogenetics/aminoacid.py
@@ -1,25 +1,36 @@
 
 from enum import Enum
+from collections import namedtuple
+
+
+Acid = namedtuple('Acid', 'duplet direction')
+
+
+class TertiaryDirection(Enum):
+    left = 'l'
+    right = 'r'
+    straight = 's'
+
 
 class AminoAcid(Enum):
-    cut = 'AC'
-    dlt = 'AG'
-    swi = 'AT'
-    mvr = 'CA'
-    mvl = 'CC'
-    cop = 'CG'
-    off = 'CT'
-    ina = 'GA'
-    inc = 'GC'
-    ing = 'GG'
-    int = 'GT'
-    tpy = 'TA'
-    rpu = 'TC'
-    lpy = 'TG'
-    lpu = 'TT'
+    cut = Acid(duplet='AC', direction=TertiaryDirection.straight)
+    dlt = Acid(duplet='AG', direction=TertiaryDirection.straight)
+    swi = Acid(duplet='AT', direction=TertiaryDirection.right)
+    mvr = Acid(duplet='CA', direction=TertiaryDirection.straight)
+    mvl = Acid(duplet='CC', direction=TertiaryDirection.straight)
+    cop = Acid(duplet='CG', direction=TertiaryDirection.right)
+    off = Acid(duplet='CT', direction=TertiaryDirection.left)
+    ina = Acid(duplet='GA', direction=TertiaryDirection.straight)
+    inc = Acid(duplet='GC', direction=TertiaryDirection.right)
+    ing = Acid(duplet='GG', direction=TertiaryDirection.right)
+    int = Acid(duplet='GT', direction=TertiaryDirection.left)
+    tpy = Acid(duplet='TA', direction=TertiaryDirection.right)
+    rpu = Acid(duplet='TC', direction=TertiaryDirection.left)
+    lpy = Acid(duplet='TG', direction=TertiaryDirection.left)
+    lpu = Acid(duplet='TT', direction=TertiaryDirection.left)
 
     @classmethod
     def get_acid_by_duplet(cls, duplet):
         for acid in cls:
-            if duplet == acid.value:
+            if duplet == acid.value.duplet:
                 return acid
--- typogenetics/tests/test_aminoacid.py
+++ typogenetics/tests/test_aminoacid.py
@@ -1,8 +1,12 @@
 
-from ..aminoacid import AminoAcid
+from ..aminoacid import AminoAcid, TertiaryDirection
 from unittest import TestCase
 
 class AminoAcidTest(TestCase):
     def test_can_get_acid_by_duplet(self):
         expected_acid = AminoAcid.dlt
         self.assertEqual(AminoAcid.get_acid_by_duplet('AG'), expected_acid)
+
+    def test_amino_acid_stores_tertiary_direction(self):
+        expected_direction = TertiaryDirection.left
+        self.assertEqual(AminoAcid.off.value.direction, expected_direction)
