ecd04f2ce6e7f363bdc255fb3da54057612b9e77
Tobal <tobal17@gmail.com>
2015-09-11 14:24:29 +0200

    refactor(ribosome): rename amino acid chain to enzyme
    Signed-off-by: Tobal <tobal17@gmail.com>

--- typogenetics/ribosome.py
+++ typogenetics/ribosome.py
@@ -3,19 +3,19 @@
 from .aminoacid import AminoAcid
 
 
-def ribosome_get_amino_acids_from_strands(strand):
+def ribosome_get_enzymes_from_strand(strand):
     if not strand:
         return
     length = len(strand) // 2
-    acid_chain = []
+    enzymes = []
     for i in range(length):
         duplet = strand[i*2:(i+1)*2]
         if duplet == 'AA':
-            yield acid_chain
-            acid_chain = []
+            yield enzymes
+            enzymes = []
         else:
-            acid_chain.append(AminoAcid.get_acid_by_duplet(duplet))
-    yield acid_chain
+            enzymes.append(AminoAcid.get_acid_by_duplet(duplet))
+    yield enzymes
 
 if __name__ == '__main__':
     pass
--- typogenetics/tests/test_ribosome.py
+++ typogenetics/tests/test_ribosome.py
@@ -1,33 +1,33 @@
 
-from ..ribosome import ribosome_get_amino_acids_from_strands
+from ..ribosome import ribosome_get_enzymes_from_strand
 from ..aminoacid import AminoAcid
 from unittest import TestCase
 
 class RibosomeTest(TestCase):
     def test_nullstrand_returns_no_amino_acids(self):
         strand = ''
-        amino_acids = list(ribosome_get_amino_acids_from_strands(strand))
+        amino_acids = list(ribosome_get_enzymes_from_strand(strand))
         self.assertEqual(len(amino_acids), 0)
 
     def test_can_convert_one_duplet_to_amino_acid(self):
         strand = 'AC'
-        amino_acids = list(ribosome_get_amino_acids_from_strands(strand))
-        self.assertListEqual([[AminoAcid.cut]], amino_acids)
+        enzymes = list(ribosome_get_enzymes_from_strand(strand))
+        self.assertListEqual([[AminoAcid.cut]], enzymes)
 
     def test_converts_long_strands_to_correct_acid_chain(self):
         strand = 'ACGCCACC'
-        amino_acids = list(ribosome_get_amino_acids_from_strands(strand))
+        enzymes = list(ribosome_get_enzymes_from_strand(strand))
         self.assertListEqual([[AminoAcid.cut, AminoAcid.inc, AminoAcid.mvr, AminoAcid.mvl]],
-                             amino_acids)
+                             enzymes)
 
     def test_converts_long_strands_with_extra_character(self):
         strand = 'ACGCCACCG'
-        amino_acids = list(ribosome_get_amino_acids_from_strands(strand))
+        enzymes = list(ribosome_get_enzymes_from_strand(strand))
         self.assertListEqual([[AminoAcid.cut, AminoAcid.inc, AminoAcid.mvr, AminoAcid.mvl]],
-                             amino_acids)
+                             enzymes)
 
     def test_can_cut_into_multiple_acid_chains(self):
         strand = 'ACGCAACACCG'
-        amino_acids = list(ribosome_get_amino_acids_from_strands(strand))
+        enzymes = list(ribosome_get_enzymes_from_strand(strand))
         self.assertListEqual([[AminoAcid.cut, AminoAcid.inc], [AminoAcid.mvr, AminoAcid.mvl]],
-                             amino_acids)
+                             enzymes)
