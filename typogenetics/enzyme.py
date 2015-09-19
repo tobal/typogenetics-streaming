473d3a6ba9cf1f25ece282c03a3d19bddc28bb13
Tobal <tobal17@gmail.com>
2015-09-19 16:06:55 +0200

    feat(enzyme): delete amino acid working
    Signed-off-by: Tobal <tobal17@gmail.com>

--- typogenetics/enzyme.py
+++ typogenetics/enzyme.py
@@ -1,6 +1,9 @@
 
+from collections import namedtuple
 from .base import Base
-from .aminoacid import TertiaryDirection
+from .aminoacid import TertiaryDirection, AminoAcid
+
+WorkingEnzyme = namedtuple('WorkingEnzyme', 'strand pos')
 
 class Enzyme:
     def __init__(self, acid_chain):
@@ -17,6 +20,13 @@ class Enzyme:
 
     def manipulate_strand(self, strand):
         strands = []
+        working_enzyme = self._bind_to_strand(strand)
+        if working_enzyme:
+            for amino_acid in self._acid_chain:
+                working_enzyme, strand = self._apply_amino_acid_on_working_enzyme(amino_acid, working_enzyme)
+                if strand:
+                    strands.append(strand)
+            strands.append(working_enzyme.strand)
         return strands
 
     def _determine_binding_preference(self):
@@ -42,3 +52,16 @@ class Enzyme:
             elif acid.value.direction == TertiaryDirection.right:
                 direction_indicator -= 1
         return direction_indicator % 4
+
+    def _bind_to_strand(self, strand):
+        for idx, symbol in enumerate(strand):
+            if Base.get_base_by_symbol(symbol) == self.binding_preference:
+                return WorkingEnzyme(strand=strand, pos=idx)
+
+    def _apply_amino_acid_on_working_enzyme(self, amino_acid,  working_enzyme):
+        strand = working_enzyme.strand
+        pos = working_enzyme.pos
+        output_strand = ''
+        if amino_acid == AminoAcid.dlt:
+            strand = strand[:pos] + strand[pos + 1:]
+        return WorkingEnzyme(strand=strand, pos=pos), output_strand
