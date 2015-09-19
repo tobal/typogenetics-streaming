643cd3923da6bebacd12312a8f50b0715c07aafa
Tobal <tobal17@gmail.com>
2015-09-19 16:45:22 +0200

    refactor(amino_acid): add apply attribute to handle acid commands
    Signed-off-by: Tobal <tobal17@gmail.com>

--- typogenetics/aminoacid.py
+++ typogenetics/aminoacid.py
@@ -1,9 +1,88 @@
 
+from .working_enzyme import WorkingEnzyme
 from enum import Enum
 from collections import namedtuple
 
 
-Acid = namedtuple('Acid', 'duplet direction')
+Acid = namedtuple('Acid', 'duplet direction apply')
+
+
+def _dlt(working_enzyme):
+    strand = working_enzyme.strand
+    pos = working_enzyme.pos
+    output_strand = ''
+
+    strand = strand[:pos] + strand[pos + 1:]
+
+    return WorkingEnzyme(strand=strand, pos=pos), output_strand
+
+
+def _swi(working_enzyme):
+    pass
+
+
+def _mvr(working_enzyme):
+    strand = working_enzyme.strand
+    pos = working_enzyme.pos
+    output_strand = ''
+
+    pos += 1
+
+    return WorkingEnzyme(strand=strand, pos=pos), output_strand
+
+
+def _mvl(working_enzyme):
+    pass
+
+
+def _cop(working_enzyme):
+    pass
+
+
+def _off(working_enzyme):
+    pass
+
+
+def _ina(working_enzyme):
+    pass
+
+
+def _inc(working_enzyme):
+    pass
+
+
+def _ing(working_enzyme):
+    pass
+
+
+def _int(working_enzyme):
+    strand = working_enzyme.strand
+    pos = working_enzyme.pos
+    output_strand = ''
+
+    strand = strand[:pos + 1] + 'T' + strand[pos + 1:]
+
+    return WorkingEnzyme(strand=strand, pos=pos), output_strand
+
+
+def _rpy(working_enzyme):
+    pass
+
+
+def _rpu(working_enzyme):
+    pass
+
+
+def _lpy(working_enzyme):
+    pass
+
+
+def _lpu(working_enzyme):
+    pass
+
+
+def _cut(working_enzyme):
+    pass
 
 
 class TertiaryDirection(Enum):
@@ -13,21 +92,21 @@ class TertiaryDirection(Enum):
 
 
 class AminoAcid(Enum):
-    cut = Acid(duplet='AC', direction=TertiaryDirection.straight)
-    dlt = Acid(duplet='AG', direction=TertiaryDirection.straight)
-    swi = Acid(duplet='AT', direction=TertiaryDirection.right)
-    mvr = Acid(duplet='CA', direction=TertiaryDirection.straight)
-    mvl = Acid(duplet='CC', direction=TertiaryDirection.straight)
-    cop = Acid(duplet='CG', direction=TertiaryDirection.right)
-    off = Acid(duplet='CT', direction=TertiaryDirection.left)
-    ina = Acid(duplet='GA', direction=TertiaryDirection.straight)
-    inc = Acid(duplet='GC', direction=TertiaryDirection.right)
-    ing = Acid(duplet='GG', direction=TertiaryDirection.right)
-    int = Acid(duplet='GT', direction=TertiaryDirection.left)
-    rpy = Acid(duplet='TA', direction=TertiaryDirection.right)
-    rpu = Acid(duplet='TC', direction=TertiaryDirection.left)
-    lpy = Acid(duplet='TG', direction=TertiaryDirection.left)
-    lpu = Acid(duplet='TT', direction=TertiaryDirection.left)
+    cut = Acid(duplet='AC', direction=TertiaryDirection.straight, apply=_cut)
+    dlt = Acid(duplet='AG', direction=TertiaryDirection.straight, apply=_dlt)
+    swi = Acid(duplet='AT', direction=TertiaryDirection.right, apply=_swi)
+    mvr = Acid(duplet='CA', direction=TertiaryDirection.straight, apply=_mvr)
+    mvl = Acid(duplet='CC', direction=TertiaryDirection.straight, apply=_mvl)
+    cop = Acid(duplet='CG', direction=TertiaryDirection.right, apply=_cop)
+    off = Acid(duplet='CT', direction=TertiaryDirection.left, apply=_off)
+    ina = Acid(duplet='GA', direction=TertiaryDirection.straight, apply=_ina)
+    inc = Acid(duplet='GC', direction=TertiaryDirection.right, apply=_inc)
+    ing = Acid(duplet='GG', direction=TertiaryDirection.right, apply=_ing)
+    int = Acid(duplet='GT', direction=TertiaryDirection.left, apply=_int)
+    rpy = Acid(duplet='TA', direction=TertiaryDirection.right, apply=_rpy)
+    rpu = Acid(duplet='TC', direction=TertiaryDirection.left, apply=_rpu)
+    lpy = Acid(duplet='TG', direction=TertiaryDirection.left, apply=_lpy)
+    lpu = Acid(duplet='TT', direction=TertiaryDirection.left, apply=_lpu)
 
     @classmethod
     def get_acid_by_duplet(cls, duplet):
--- typogenetics/tests/test_aminoacid.py
+++ typogenetics/tests/test_aminoacid.py
@@ -11,3 +11,27 @@ class AminoAcidTest(TestCase):
     def test_amino_acid_stores_tertiary_direction(self):
         expected_direction = TertiaryDirection.left
         self.assertEqual(AminoAcid.off.value.direction, expected_direction)
+
+    def test_dlt(self):
+        input_working_enzyme = WorkingEnzyme(strand='ATA', pos=1)
+        delete = AminoAcid.dlt
+        output_working_enzyme, output_strand = delete.value.apply(input_working_enzyme)
+        expected_working_enzyme = WorkingEnzyme(strand='AA', pos=1)
+        self.assertEqual(expected_working_enzyme, output_working_enzyme)
+        self.assertEqual('', output_strand)
+
+    def test_mvr(self):
+        input_working_enzyme = WorkingEnzyme(strand='ATA', pos=1)
+        move_right = AminoAcid.mvr
+        output_working_enzyme, output_strand = move_right.value.apply(input_working_enzyme)
+        expected_working_enzyme = WorkingEnzyme(strand='ATA', pos=2)
+        self.assertEqual(expected_working_enzyme, output_working_enzyme)
+        self.assertEqual('', output_strand)
+
+    def test_int(self):
+        input_working_enzyme = WorkingEnzyme(strand='ATA', pos=1)
+        insert_t = AminoAcid.int
+        output_working_enzyme, output_strand = insert_t.value.apply(input_working_enzyme)
+        expected_working_enzyme = WorkingEnzyme(strand='ATTA', pos=1)
+        self.assertEqual(expected_working_enzyme, output_working_enzyme)
+        self.assertEqual('', output_strand)
